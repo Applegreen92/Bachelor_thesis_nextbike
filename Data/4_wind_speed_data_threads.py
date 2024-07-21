16f9828acbe1d484cded03f227a5894cd208a3ba
David <david.rumpf@stud.uni-due.de>
2024-07-21 19:33:44 +0200

    add anothr city

--- Data/4_wind_speed_data_threads.py
+++ Data/4_wind_speed_data_threads.py
@@ -8,7 +8,7 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 start_time = time.time()
 # Load the CSV file containing bike availability and station information
-csv_file_path = 'preprocessed_data/cloudCover_temp_bike_station_data_heidelberg.csv'
+csv_file_path = 'preprocessed_data/cloudCover_temp_bike_station_data_dresden.csv'
 csv_df = pd.read_csv(csv_file_path)
 
 # Convert 'datetime' column to datetime objects
